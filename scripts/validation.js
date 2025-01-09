96988284d5438cee25254d022872a92fcfc33982
Manahil Sami <manahil_sami@outlook.com>
2025-01-08 23:00:37 -0500

    red outline for error on input added

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,11 +1,13 @@
 const showInputError = (formEl, inputEl, errorMsg) => {
   const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
   errorMsgEl.textContent = errorMsg;
+  inputEl.classList.add("modal__input_type_error");
 };
 
 const hideInputError = (formEl, inputEl) => {
   const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
   errorMsgEl.textContent = "";
+  inputEl.classList.remove("modal__input_type_error");
 };
 
 const checkInputValidity = (formEl, inputEl) => {

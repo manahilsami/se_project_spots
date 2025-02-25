7ffbac8f99e6553ab710003782c82c93224183dc
Manahil Sami <manahil_sami@outlook.com>
2025-02-24 20:35:04 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
+import "./index.css";
+import {
+  enableValidation,
+  settings,
+  resetValidation,
+  disableButton,
+} from "../scripts/validation.js";
+
 const initialCards = [
   {
     name: "Val Thorens",
@@ -174,3 +182,5 @@ initialCards.forEach((item) => {
   const cardElement = getCardElement(item);
   cardsList.prepend(cardElement);
 });
+
+enableValidation(settings);
